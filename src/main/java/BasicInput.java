b13251b3fca18f5d42c3e289582651debe44d227
dtross20 <dtross20@students.desu.edu>
2024-01-24 16:34:33 -0500

    Defined Char and String variables Completed Todo 1 Task

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -9,15 +9,22 @@ public class BasicInput {
         char userChar = ' ';
         String userString = "";
 
-
-
-
         System.out.println("Enter integer:");
         userInt = scnr.nextInt();
 
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
+        userDouble = scnr.nextDouble();
+        userChar = scnr.next().charAt(0);
+        userString = scnr.nextLine();
+
+        System.out.print(userDouble);
+        System.out.print(userInt);
+        System.out.print(userChar);
+        System.out.print(userString);
+
 
         // TODO (2): Output the four values in reverse
+
 
         // TODO (3): Cast the double to an integer, and output that integer
 
